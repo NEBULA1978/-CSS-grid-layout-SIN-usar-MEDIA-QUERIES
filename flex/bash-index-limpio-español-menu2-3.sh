bb4e0a391bd013fa758a7a6d5a1624920cafcb5a
NEBULA1978 <rpascualey@gmail.com>
2023-10-30 19:18:42 +0100

    Pasando a limpio flex css añado variables css con root falta ver valores a ingresar

--- flex/bash-index-limpio-español-menu2-3.sh
+++ flex/bash-index-limpio-español-menu2-3.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
 # Define opciones sugeridas para cada variable o propiedad CSS
-container_bg_options="burlywood white lightgray"
-container_padding_options="10px 20px 5px"
-container_border_options="2px solid black 1px dashed gray"
-child_bg_options="gainsboro white lightblue"
-child_width_options="100px 150px 80px"
-child_margin_options="10px 15px 5px"
-child_text_align_options="center left right"
-child_line_height_options="75px 80px 60px"
-child_font_size_options="30px 24px 36px"
-child_border_options="2px solid black 1px dotted gray"
-box_sizing_options="border-box content-box"
-flex_container_display_options="flex inline-flex"
-flex_container_flex_flow_options="row wrap column nowrap"
-flex_container_justify_content_options="space-between flex-start center"
-flex_container_align_items_options="stretch center flex-start"
+declare -A options
+options["container-bg"]="burlywood white lightgray"
+options["container-padding"]="10px 20px 5px"
+options["container-border"]="2px solid black 1px dashed gray"
+options["child-bg"]="gainsboro white lightblue"
+options["child-width"]="100px 150px 80px"
+options["child-margin"]="10px 15px 5px"
+options["child-text-align"]="center left right"
+options["child-line-height"]="75px 80px 60px"
+options["child-font-size"]="30px 24px 36px"
+options["child-border"]="2px solid black 1px dotted gray"
+options["box-sizing"]="border-box content-box"
+options["flex-container-display"]="flex inline-flex"
+options["flex-container-flex-flow"]="row wrap column nowrap"
+options["flex-container-justify-content"]="space-between flex-start center"
+options["flex-container-align-items"]="stretch center flex-start"
 
 # Función para mostrar opciones y obtener selección
 select_option() {
-    local options=("$@")
-    local option_count=${#options[@]}
+    local var_name="$1"
+    local current_value="${options[$var_name]}"
+    local options_list="${options[$var_name]}"
+    local options_array=($options_list)
 
-    for ((i = 0; i < option_count; i++)); do
-        echo "$i. ${options[i]}"
+    echo "Valor actual para $var_name: $current_value"
+    echo "Opciones disponibles:"
+    for ((i = 0; i < ${#options_array[@]}; i++)); do
+        echo "$i. ${options_array[i]}"
     done
 
-    read -p "Selecciona una opción (0-$((option_count - 1))): " selected_option
+    read -p "Selecciona una opción (0-$((i - 1))) o presiona Enter para usar el valor actual: " selected_option
 
-    if [[ $selected_option -ge 0 && $selected_option -lt $option_count ]]; then
-        echo "${options[selected_option]}"
+    if [[ -z "$selected_option" || ($selected_option -ge 0 && $selected_option -lt ${#options_array[@]}) ]]; then
+        echo "${options_array[$selected_option]}"
     else
-        echo "${options[0]}"
+        echo "$current_value"
     fi
 }
 
@@ -45,38 +50,16 @@ read -p "Opción: " choice
 # Comprueba la elección del usuario
 if [ "$choice" == "0" ]; then
     # Valores por defecto
-    container_bg=$(select_option $container_bg_options)
-    container_padding=$(select_option $container_padding_options)
-    container_border=$(select_option $container_border_options)
-    child_bg=$(select_option $child_bg_options)
-    child_width=$(select_option $child_width_options)
-    child_margin=$(select_option $child_margin_options)
-    child_text_align=$(select_option $child_text_align_options)
-    child_line_height=$(select_option $child_line_height_options)
-    child_font_size=$(select_option $child_font_size_options)
-    child_border=$(select_option $child_border_options)
-    box_sizing=$(select_option $box_sizing_options)
-    flex_container_display=$(select_option $flex_container_display_options)
-    flex_container_flex_flow=$(select_option $flex_container_flex_flow_options)
-    flex_container_justify_content=$(select_option $flex_container_justify_content_options)
-    flex_container_align_items=$(select_option $flex_container_align_items_options)
+    declare -A default_values
+    for var_name in "${!options[@]}"; do
+        default_values["$var_name"]=$(echo "${options[$var_name]}" | cut -d ' ' -f 1)
+    done
+
 else
     # Personalizar valores
-    read -p "Nuevo valor para --container-bg (color de fondo del contenedor): " container_bg
-    read -p "Nuevo valor para --container-padding (espaciado interior del contenedor): " container_padding
-    read -p "Nuevo valor para --container-border (borde del contenedor): " container_border
-    read -p "Nuevo valor para --child-bg (color de fondo de elementos hijos): " child_bg
-    read -p "Nuevo valor para --child-width (ancho de elementos hijos): " child_width
-    read -p "Nuevo valor para --child-margin (margen de elementos hijos): " child_margin
-    read -p "Nuevo valor para --child-text-align (alineación de texto de elementos hijos): " child_text_align
-    read -p "Nuevo valor para --child-line-height (altura de línea de elementos hijos): " child_line_height
-    read -p "Nuevo valor para --child-font-size (tamaño de fuente de elementos hijos): " child_font_size
-    read -p "Nuevo valor para --child-border (borde de elementos hijos): " child_border
-    read -p "Nuevo valor para --box-sizing (modelo de caja): " box_sizing
-    read -p "Nuevo valor para display de .flex-container: " flex_container_display
-    read -p "Nuevo valor para flex-flow de .flex-container: " flex_container_flex_flow
-    read -p "Nuevo valor para justify-content de .flex-container: " flex_container_justify_content
-    read -p "Nuevo valor para align-items de .flex-container: " flex_container_align_items
+    for var_name in "${!options[@]}"; do
+        options["$var_name"]=$(select_option "$var_name")
+    done
 fi
 
 # Crea un archivo HTML con las variables y propiedades actualizadas
@@ -90,18 +73,18 @@ cat > index-limpio-espanol-bash-con-variables.html <<EOF
 
     <style>
         :root {
-            --box-sizing: $box_sizing;
-            --container-bg: $container_bg;
-            --container-padding: $container_padding;
-            --container-border: $container_border;
+            --box-sizing: ${options["box-sizing"]};
+            --container-bg: ${options["container-bg"]};
+            --container-padding: ${options["container-padding"]};
+            --container-border: ${options["container-border"]};
 
-            --child-bg: $child_bg;
-            --child-width: $child_width;
-            --child-margin: $child_margin;
-            --child-text-align: $child_text_align;
-            --child-line-height: $child_line_height;
-            --child-font-size: $child_font_size;
-            --child-border: $child_border;
+            --child-bg: ${options["child-bg"]};
+            --child-width: ${options["child-width"]};
+            --child-margin: ${options["child-margin"]};
+            --child-text-align: ${options["child-text-align"]};
+            --child-line-height: ${options["child-line-height"]};
+            --child-font-size: ${options["child-font-size"]};
+            --child-border: ${options["child-border"]};
         }
 
         * {
@@ -109,10 +92,10 @@ cat > index-limpio-espanol-bash-con-variables.html <<EOF
         }
 
         .flex-container {
-            display: $flex_container_display;
-            flex-flow: $flex_container_flex_flow;
-            justify-content: $flex_container_justify_content;
-            align-items: $flex_container_align_items;
+            display: ${options["flex-container-display"]};
+            flex-flow: ${options["flex-container-flex-flow"]};
+            justify-content: ${options["flex-container-justify-content"]};
+            align-items: ${options["flex-container-align-items"]};
             background-color: var(--container-bg);
             padding: var(--container-padding);
             border: var(--container-border);
@@ -120,7 +103,7 @@ cat > index-limpio-espanol-bash-con-variables.html <<EOF
 
         .flex-container div {
             background-color: var(--child-bg);
-            width: var(--child-width);
+            width: var(--child-width};
             margin: var(--child-margin);
             text-align: var(--child-text-align);
             line-height: var(--child-line-height);
@@ -130,7 +113,7 @@ cat > index-limpio-espanol-bash-con-variables.html <<EOF
     </style>
 </head>
 <body>
-<div class="flex-container">
+<div class "flex-container">
     <div>1</div>
     <div>2</div>
     <div>3</div>
